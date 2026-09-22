49b99a59cc9bdcb129954b05e3180931bd5a8e35
rebrops-del <rebrops@gmail.com>
2026-09-22 16:10:33 +0500

    Configure Yandex Metrika counter 112921323

--- assets/yandex-metrika-config.js
+++ assets/yandex-metrika-config.js
@@ -1,3 +1,2 @@
 // NOKTENA — Yandex Metrika configuration.
-// Set the numeric counter ID here after the counter is created in Yandex Metrika.
-window.NOKTENA_METRIKA_ID = Number(window.NOKTENA_METRIKA_ID || 0);
+window.NOKTENA_METRIKA_ID = 112921323;
